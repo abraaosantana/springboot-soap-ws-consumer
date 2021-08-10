8fbf19977d1a6e45feea814e807663d04c589285
Abraão Santana <deploys.app@gmail.com>
2021-08-09 23:19:44 -0300

    Criação projeto

---
+++ src/main/java/br/com/devweb/ws/SpringbootSoapWsConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.com.devweb.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSoapWsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSoapWsConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devweb/ws/SpringbootSoapWsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devweb.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSoapWsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
